c28e28eec53cd6a1594efe57b4db2a242160bc63
PikaKight <tmmarcus73@gmail.com>
2020-08-26 22:00:59 -0400

    can use django with html

--- Django/hello/views.py
+++ Django/hello/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    # return HttpResponse("Hello, World!")
+    return render(request, "hello/index.html")
 
 def brian(request):
     return HttpResponse("Hello, Brian!")
@@ -13,4 +14,5 @@ def david(request):
     return HttpResponse("Hello, David!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name}!")
+    # return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {"name": name.capitalize()})
